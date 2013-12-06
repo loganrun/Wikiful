c7b3221115911c9983c7a382fd13ce1b6bbe5bf6
OpenShift guest <bigdog_41@hotmail.com>
2013-12-06 12:59:30 -0500

    add pictures

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Wikiful::Application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  get "articles/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
